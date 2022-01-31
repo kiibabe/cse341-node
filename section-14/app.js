e4676b5d7d1ae1a863eec4204178f742334d6bf4
kiibabe <51504168+kiibabe@users.noreply.github.com>
2022-01-31 23:33:55 +0900

    Week 05

---
+++ section-14/app.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const PATH = process.env.PORT || 5000;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const cors = require('cors');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://admin:rOEtZCt9TCcf1g1a@cluster0.dp8pa.mongodb.net/Shop?retryWrites=true&w=majority";
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+const corsOptions = {
+  origin: "https://cse341-node-kisetsu.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  family: 4
+};
+
+mongoose
+  .connect(MONGODB_URL, options)
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(PATH);
+  })
+  .catch(err => {
+    console.log(err);
+  });
