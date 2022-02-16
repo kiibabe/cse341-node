5c2e8e5bd683f59fcc2aa63176bd217d930ebb51
kiibabe <51504168+kiibabe@users.noreply.github.com>
2022-02-16 20:59:55 +0900

    week 07

--- team/app.js
+++ team/app.js
@@ -7,10 +7,77 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
-const cors = require('cors');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://admin:rOEtZCt9TCcf1g1a@cluster0.dp8pa.mongodb.net/Shop?retryWrites=true&w=majority";
 
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URL,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
+
+app.use(authRoutes);
+app.get('/500', errorController.get500);
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(result => {
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- team/controllers/auth.js
+++ team/controllers/auth.js
@@ -1,20 +1,15 @@
 const crypto = require('crypto');
-
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
-const sendgridTransport = require('nodemailer-sendgrid-transport');
 const { validationResult } = require('express-validator/check');
-
 const User = require('../models/user');
 
-// const transporter = nodemailer.createTransport(
-//   sendgridTransport({
-//     auth: {
-//       api_key:
-//         ''
-//     }
-//   })
-// );
+exports.getIndex = (req, res, next) => {
+    res.render('shop/index', {
+        pageTitle: 'Home',
+        path: '/'
+      });
+};
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -140,7 +135,6 @@ exports.postSignup = (req, res, next) => {
       validationErrors: errors.array()
     });
   }
-
   bcrypt
     .hash(password, 12)
     .then(hashedPassword => {
@@ -150,28 +144,20 @@ exports.postSignup = (req, res, next) => {
         cart: { items: [] }
       });
       return user.save()
-      .then(result => {
+    .then(result => {
         req.session.isLoggedIn = true;
         req.session.user = user;
         return req.session.save(err => {
           console.log(err);
           res.redirect('/');
-      });
-      })
-    // .then(result => {
-    //   res.redirect('/login');
-      // return transporter.sendMail({
-      //   to: email,
-      //   from: 'kisetsuirie@gmail.com',
-      //   subject: 'Signup succeeded!',
-      //   html: '<h1>You successfully signed up!</h1>'
-      // });
+    });
+  })
     })
     .catch(err => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
-    });
+  });
 };
 
 exports.postLogout = (req, res, next) => {
@@ -179,107 +165,4 @@ exports.postLogout = (req, res, next) => {
     console.log(err);
     res.redirect('/');
   });
-};
-
-exports.getReset = (req, res, next) => {
-  let message = req.flash('error');
-  if (message.length > 0) {
-    message = message[0];
-  } else {
-    message = null;
-  }
-  res.render('auth/reset', {
-    path: '/reset',
-    pageTitle: 'Reset Password',
-    errorMessage: message,
-    resetMessage: null
-  });
-};
-
-exports.postReset = (req, res, next) => {
-  crypto.randomBytes(32, (err, buffer) => {
-    if (err) {
-      console.log(err);
-      return res.redirect('/reset');
-    }
-    const token = buffer.toString('hex');
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (!user) {
-          req.flash('error', 'No account with that email found.');
-          return res.redirect('/reset');
-        }
-        user.resetToken = token;
-        user.resetTokenExpiration = Date.now() + 3600000;
-        user.save();
-        res.render('auth/reset', {
-          path: '/reset',
-          pageTitle: 'Reset Password',
-          email: req.body.email,
-          errorMessage: null,
-          resetMessage: `reset/${token}`
-        })
-      })
-      .catch(err => {
-        const error = new Error(err);
-        error.httpStatusCode = 500;
-        return next(error);
-      });
-  });
-};
-
-exports.getNewPassword = (req, res, next) => {
-  const token = req.params.token;
-  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
-    .then(user => {
-      let message = req.flash('error');
-      if (message.length > 0) {
-        message = message[0];
-      } else {
-        message = null;
-      }
-      res.render('auth/new-password', {
-        path: '/new-password',
-        pageTitle: 'New Password',
-        errorMessage: message,
-        userId: user._id.toString(),
-        passwordToken: token
-      });
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
-    });
-};
-
-exports.postNewPassword = (req, res, next) => {
-  const newPassword = req.body.password;
-  const userId = req.body.userId;
-  const passwordToken = req.body.passwordToken;
-  let resetUser;
-
-  User.findOne({
-    resetToken: passwordToken,
-    resetTokenExpiration: { $gt: Date.now() },
-    _id: userId
-  })
-    .then(user => {
-      resetUser = user;
-      return bcrypt.hash(newPassword, 12);
-    })
-    .then(hashedPassword => {
-      resetUser.password = hashedPassword;
-      resetUser.resetToken = undefined;
-      resetUser.resetTokenExpiration = undefined;
-      return resetUser.save();
-    })
-    .then(result => {
-      res.redirect('/login');
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
-    });
 };
